0366ee2f9f829bb08f958117894b884e1f722788
smiracle18 <serenesushant@gmail.com>
2022-12-26 17:11:45 +0530

    modified server url to live server

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:5000/',{
+  const response = await fetch('https://codex-ai-6gb7.onrender.com/',{
     method: 'POST',
     headers: {
       'Content-type': 'application/json'
